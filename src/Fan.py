0e4144f0f7d5e5d875c342be3c32998a00de09bc
Mani Deepak <mani090806@gmail.com>
2025-06-14 00:05:48 +0530

    Made changes in fan

--- src/Fan.py
+++ src/Fan.py
@@ -4,6 +4,19 @@ class Fan(SmartDevice):
     def __init__(self, name):
         super().__init__(name)
         self.speed = 0 
+    
+    def turn_on(self,speed):
+        if speed < 0 or speed > 5:
+            print(f"{self.name}: Invalid speed! Please set between 0 and 5.")
+            return
+        self.status = "ON"
+        self.speed=speed
+        print(f"{self.name} turned ON with speed {self.speed}")
+        
+    def turn_off(self):
+        self.status = "OFF"
+        self.speed=0
+        print(f"{self.name} turned OFF")
 
     def set_speed(self, speed):
         if speed < 0 or speed > 5:
@@ -14,10 +27,3 @@ class Fan(SmartDevice):
 
     def __str__(self):
         return f"{self.name} is currently {self.status}, speed: {self.speed}"
-
-    
-    
-
-    
-
-    
